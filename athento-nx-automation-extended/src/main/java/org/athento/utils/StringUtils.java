92d3b77debaad4f9dad1ea6c2cd0f243f2d44994
Paco Alías <pa@athento.com>
2016-06-21 17:15:36 +0200

    Now you can watch for more than 1 docType and launch any operation available

--- athento-nx-automation-extended/src/main/java/org/athento/utils/StringUtils.java
+++ athento-nx-automation-extended/src/main/java/org/athento/utils/StringUtils.java
@@ -6,6 +6,10 @@ package org.athento.utils;
 
 import java.io.Serializable;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.athento.nuxeo.operations.AthentoDocumentUpdateOperation;
+
 /**
  * @author athento
  *
@@ -14,14 +18,15 @@ public class StringUtils {
 
 	public static final String AND_WITH_SPACES = " AND ";
 	public static final String BLANK = " ";
+	public static final String COMMA = ",";
 	public static final String EMPTY = "";
-	public static final Object EQUALS_WITH_SPACES = " = ";
+	public static final String EQUALS_WITH_SPACES = " = ";
 	public static final String OR_WITH_SPACES = " OR ";
 	public static final String SINGLE_QUOTE = "'";
-	public static final Object NULL = "null";
-	public static final Object IS_WITH_SPACES = " is ";
-	public static final Object PARENTHESIS_LEFT = "(";
-	public static final Object PARENTHESIS_RIGHT = ")";
+	public static final String NULL = "null";
+	public static final String IS_WITH_SPACES = " is ";
+	public static final String PARENTHESIS_LEFT = "(";
+	public static final String PARENTHESIS_RIGHT = ")";
 	
 	public static String getLastField (String path, String delimiter){
 		String[] pieces = path.split(delimiter);
@@ -31,11 +36,6 @@ public class StringUtils {
 		return path;
 	}
 	
-	public static boolean isNullOrEmpty (String s) {
-		if (s == null) return true;
-		return s.isEmpty();
-	}
-
 	public static String getValue(Serializable value) {
 		if (value != null) return value.toString();
 		return null;
@@ -50,4 +50,28 @@ public class StringUtils {
 		}
 		return rtn;
 	}
+
+    public static boolean isNullOrEmpty (String s) {
+    	if (s == null) return true;
+    	return s.isEmpty();
+    }
+    
+    public static boolean isIncludedIn (String value, String chain, String delimiter) {
+        try {
+            String[] pieces = chain.split(delimiter);
+            for (int i=0; i < pieces.length; i++) {
+                String s = pieces[i].trim();
+                if (s.equals(value)) {
+                    return true;
+                }
+            }
+        } catch (Throwable t) {
+            _log.error("Invalid values for processing: " + t.getMessage());
+            _log.error(" value: " + value);
+            _log.error(" chain: " + chain);
+            _log.error(" delimiter: " + delimiter);
+        }
+        return false;
+    }
+    private static final Log _log = LogFactory.getLog(StringUtils.class);
 }
