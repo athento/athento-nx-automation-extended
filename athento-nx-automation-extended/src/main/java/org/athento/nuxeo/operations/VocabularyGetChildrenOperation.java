d4d0fea4cff504caa58e32e75d9708d2b68b783b
vs_ <vs@athento.com>
2016-08-06 11:48:21 +0200

    AT-975 Filter vocabulary children multi parent value, sorted with limit

--- athento-nx-automation-extended/src/main/java/org/athento/nuxeo/operations/VocabularyGetChildrenOperation.java
+++ athento-nx-automation-extended/src/main/java/org/athento/nuxeo/operations/VocabularyGetChildrenOperation.java
@@ -1,18 +1,7 @@
-/**
- * 
- */
 package org.athento.nuxeo.operations;
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-
 import net.sf.json.JSONObject;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.common.utils.i18n.I18NUtils;
@@ -28,123 +17,201 @@ import org.nuxeo.ecm.directory.Session;
 import org.nuxeo.ecm.directory.api.DirectoryService;
 import org.nuxeo.runtime.api.Framework;
 
+import java.util.*;
+
 /**
- * @author athento
+ * Get vocabulary children. It is able to get several parents to filter, and order with limit and offset.
  *
+ * @author victorsanchez
  */
 @Operation(id = VocabularyGetChildrenOperation.ID, category = "Athento", label = "Vocabulary Get Children", description = "Returns a list of children vocabularies of specified vocabulary by param 'name'")
 public class VocabularyGetChildrenOperation {
 
-	public static final String ID = "Athento.VocabularyGetChildrenOperation";
+    public static final String ID = "Athento.VocabularyGetChildrenOperation";
 
-	@Context
-	protected OperationContext ctx;
+    @Context
+    protected OperationContext ctx;
 
-	@Param(name = "expanded", widget = Constants.W_CHECK, required=false)
-	protected Boolean expanded = false;
-	@Param(name = "languageId", widget = Constants.W_TEXT, required=false)
-	protected String languageId;
-	@Param(name = "parentValue", widget = Constants.W_TEXT)
-	protected String parentValue;
-	@Param(name = "vocabularyName", widget = Constants.W_TEXT)
-	protected String vocabularyName;
+    @Param(name = "expanded", widget = Constants.W_CHECK, required = false)
+    protected Boolean expanded = false;
+    @Param(name = "languageId", widget = Constants.W_TEXT, required = false)
+    protected String languageId;
+    /**
+     * This value is able to get multiple values, separated by comma.
+     */
+    @Param(name = "parentValue", widget = Constants.W_TEXT, description = "You can including multiple parent values separated by comma")
+    protected String parentValue;
+    @Param(name = "vocabularyName", widget = Constants.W_TEXT)
+    protected String vocabularyName;
+    /**
+     * Format of orderBy field:ASC|DESC.
+     */
+    @Param(name = "orderBy", required = false,
+                             description = "You can use order-by incluiding several columns to order separated by comman. If you set more than one parent value you only can use one column as order.")
+    protected String orderBy;
+    @Param(name = "limit", required = false)
+    protected int limit = -1;
+    @Param(name = "offset", required = false)
+    protected int offset = -1;
 
-	/*
-	 * vocabularyName = 'country'
-	 * parentValue = 'africa'
-	 */
-	
-	@OperationMethod
-	public Object run() throws Exception {
-		if (_log.isDebugEnabled()) {
-			_log.debug("Getting children of vocabulary: " + vocabularyName + " for value " + parentValue);
-			_log.debug("    languageId: " + languageId + " expanded:" + expanded);
-		}
-		List<JSONObject> children = getValuesForParentValue();
-		if (_log.isDebugEnabled()) {
-			_log.debug("Children vocabularies of [" + vocabularyName + "]: " + children);
-		}
+    /**
+     * Run operation.
+     *
+     * @return
+     * @throws Exception
+     */
+    @OperationMethod
+    public Object run() throws Exception {
+        if (_log.isDebugEnabled()) {
+            _log.debug("Getting children of vocabulary: " + vocabularyName + " for value " + parentValue);
+            _log.debug("    languageId: " + languageId + " expanded:" + expanded);
+        }
+        List<JSONObject> children = getValuesForParentValue();
+        if (_log.isDebugEnabled()) {
+            _log.debug("Children vocabularies of [" + vocabularyName + "]: " + children);
+        }
 
-		return children;
-	}
-	
-	private List<JSONObject> getValuesForParentValue() {
-		
-		List<JSONObject> result = new ArrayList<JSONObject>();
-		Session directorySession = getDirectoryService().open(vocabularyName);
+        return children;
+    }
 
-		Map queryFilter = new HashMap();
-		queryFilter.put("parent", parentValue);
-		if (_log.isDebugEnabled()) {
-			_log.debug("Querying directory [" + vocabularyName + "] with query: " + queryFilter);
-		}
+    /**
+     * Get parent for parent value.
+     *
+     * @return list of JSON objects
+     * @since AT-975
+     */
+    private List<JSONObject> getValuesForParentValue() {
+        List<DocumentModel> result = new ArrayList<DocumentModel>();
+        List<JSONObject> jsonResult = new ArrayList<>();
+        Session directorySession = getDirectoryService().open(vocabularyName);
+        try {
+            if (parentValue.contains(",")) {
+                String[] parentValues = parentValue.split(",");
+                for (String parentValue : parentValues) {
+                    result.addAll(filterForParentValue(directorySession, parentValue.trim()));
+                }
+            } else {
+                result.addAll(filterForParentValue(directorySession, parentValue));
+            }
+        } finally {
+            directorySession.close();
+        }
+        if (!orderBy.contains(",")) {
+            Collections.sort(result, new Comparator<DocumentModel>() {
+                @Override
+                public int compare(DocumentModel d1, DocumentModel d2) {
+                    Object value1 = d1.getPropertyValue(orderBy);
+                    if (value1 instanceof Comparable) {
+                        Object value2 = d2.getPropertyValue(orderBy);
+                        return ((Comparable) value1).compareTo(value2);
+                    } else {
+                        return 0;
+                    }
+                }
+            });
+        }
+        // Set locale
+        Locale locale = null;
+        if (languageId != null) {
+            locale = new Locale(languageId);
+        }
+        // Complete JSON result
+        Iterator<DocumentModel> it = result.iterator();
+        while (it.hasNext()) {
+            DocumentModel entry = it.next();
+            String label = (String) entry.getPropertyValue("xvocabulary:label");
+            if (_log.isDebugEnabled()) {
+                _log.debug("- entry Id: " + entry.getId());
+                _log.debug("- entry Title: " + entry.getTitle());
+                _log.debug("- entry Label: " + label);
+            }
 
-		DocumentModelList queryResult = directorySession.query(queryFilter);
-		Locale locale = null;
-		if (languageId  != null) {
-			locale = new Locale(languageId);
-		}
-		Iterator<DocumentModel> it = queryResult.iterator();
-		while (it.hasNext()) {
-			DocumentModel entry = it.next();
-			String label = (String) entry.getPropertyValue("xvocabulary:label");
-			if (_log.isDebugEnabled()) {
-				_log.debug("- entry Id: " + entry.getId());
-				_log.debug("- entry Title: " + entry.getTitle());
-				_log.debug("- entry Label: " + label);
-			}
+            if (locale != null) {
+                if (label != null) {
+                    label = I18NUtils.getMessageString(
+                            "messages", label, new String[0], locale);
+                }
+            }
+            JSONObject val = new JSONObject();
+            if (!expanded) {
+                val.put(entry.getId(), label);
+            } else {
+                val.put("id", entry.getId());
+                val.put("label", label);
+            }
+            if (_log.isDebugEnabled()) {
+                _log.debug(">> adding value: " + val);
+            }
+            jsonResult.add(val);
+        }
+        return jsonResult;
+    }
 
-			if (locale != null) {
-				if (label != null) {
-					label = I18NUtils.getMessageString(
-						"messages", label, new String[0], locale);
-				}
-			}
-			JSONObject val = new JSONObject();
-			if (!expanded) {
-				val.put(entry.getId(), label);
-			} else {
-				val.put("id", entry.getId());
-				val.put("label", label);
-			}
-			if (_log.isDebugEnabled()) {
-				_log.debug(">> adding value: " + val);
-			}
-			result.add(val);
-		}
-		directorySession.close();
+    /**
+     * Add values filtering for a parent value.
+     *
+     * @param directorySession is the session
+     * @param parentValue      is the parent value to filter
+     * @return list of JSON objects
+     */
+    private List<DocumentModel> filterForParentValue(Session directorySession, String parentValue) {
+        List<JSONObject> result = new ArrayList<JSONObject>();
+        // Prepare filter map
+        Map queryFilter = new HashMap();
+        queryFilter.put("parent", parentValue);
+        if (_log.isInfoEnabled()) {
+            _log.info("Querying directory [" + vocabularyName + "] with query: " + queryFilter);
+        }
+        // Get sort info
+        Map<String, String> sortInfo = getSortInfo();
+        // Execute query
+        return directorySession.query(queryFilter, new HashSet<String>(0), sortInfo
+                , false, limit, offset);
+    }
 
-		return result;
-	}
+    /**
+     * Get sort info.
+     *
+     * @return map of order by's
+     */
+    private Map<String, String> getSortInfo() {
+        Map<String, String> sortInfo = new HashMap<String, String>();
+        if (this.orderBy != null) {
+            if (this.orderBy.contains(",")) {
+                String[] multipleOrderBy = this.orderBy.split(",");
+                for (String orderBy : multipleOrderBy) {
+                    getOrderByInfo(orderBy.trim(), sortInfo);
+                }
+            }
+        }
+        return sortInfo;
+    }
 
-//	private List<Directory> getChildren(String name2) {
-//		List<String> directories = getDirectoryService().getDirectoryNames();
-//
-//		List<Directory> children = new ArrayList<Directory>();
-//		for (String dir: directories) {
-//			String parentDir = getDirectoryService().getParentDirectoryName(dir);
-//
-//			if (_log.isDebugEnabled()) {
-//				_log.debug("directory: " + dir + " parent: " + parentDir + " parentId: -");
-//			}
-//			if (name.equals(parentDir)) {
-//				_log.debug(" >> adding child: " + dir);
-//				children.add(getDirectoryService().getDirectory(dir));
-//			}
-//		}
-//		return children;
-//	}
+    /**
+     * Get order-by info.
+     *
+     * @param orderBy
+     * @param sortInfo
+     */
+    private void getOrderByInfo(String orderBy, Map<String, String> sortInfo) {
+        String[] orderByInfo = orderBy.split(":");
+        if (orderByInfo.length > 1) {
+            sortInfo.put(orderByInfo[0].trim(), orderByInfo[1].trim());
+        } else {
+            sortInfo.put(orderBy, "ASC");
+        }
+    }
 
-	private DirectoryService getDirectoryService() {
-		if (directoryService == null) {
-			directoryService = Framework.getService(DirectoryService.class);
-		}
-		return directoryService;
-	}
-	
-	private DirectoryService directoryService;
+    private DirectoryService getDirectoryService() {
+        if (directoryService == null) {
+            directoryService = Framework.getService(DirectoryService.class);
+        }
+        return directoryService;
+    }
 
-	
-	private static final Log _log = LogFactory.getLog(VocabularyGetChildrenOperation.class);
+    private DirectoryService directoryService;
+
+
+    private static final Log _log = LogFactory.getLog(VocabularyGetChildrenOperation.class);
 
 }
