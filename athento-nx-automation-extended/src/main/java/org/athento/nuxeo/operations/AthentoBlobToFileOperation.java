8293d0cea47ad10d5ca1312ba098d0763bbbee42
vs_ <vs@athento.com>
2017-10-26 12:18:47 +0200

    Add Blob.ToFile operation

---
+++ athento-nx-automation-extended/src/main/java/org/athento/nuxeo/operations/AthentoBlobToFileOperation.java
@@ -0,0 +1,60 @@
+package org.athento.nuxeo.operations;
+
+import org.nuxeo.ecm.automation.core.Constants;
+import org.nuxeo.ecm.automation.core.annotations.Operation;
+import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.automation.core.annotations.Param;
+import org.nuxeo.ecm.automation.core.collectors.BlobCollector;
+import org.nuxeo.ecm.automation.core.operations.blob.BlobToFile;
+import org.nuxeo.ecm.core.api.Blob;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Athento Blob to File using a filename parameter.
+ */
+@Operation(id = BlobToFile.ID, category = "Athento", label = "Export to File", description = "Save blob into file.")
+public class AthentoBlobToFileOperation {
+
+    public static final String ID = "Athento.BlobToFile";
+
+    @Param(name = "directory")
+    protected String directory;
+
+    @Param(name = "filename", required = false)
+    protected String filename;
+
+    protected File root;
+
+    protected void init() {
+        root = new File(directory);
+        root.mkdirs();
+    }
+
+    protected File getFile(String name) {
+        return new File(root, name);
+    }
+
+    protected void writeFile(Blob blob) throws IOException {
+        String name = blob.getFilename();
+        if (name.length() == 0) {
+            name = "blob#" + Integer.toHexString(System.identityHashCode(blob));
+        }
+        if (filename == null) {
+            filename = name;
+        }
+        File file = getFile(filename);
+        File tmp = new File(file.getParentFile(), file.getName() + ".tmp");
+        blob.transferTo(tmp);
+        tmp.renameTo(file);
+    }
+
+    @OperationMethod(collector = BlobCollector.class)
+    public Blob run(Blob blob) throws Exception {
+        init();
+        writeFile(blob);
+        return blob;
+    }
+
+}
