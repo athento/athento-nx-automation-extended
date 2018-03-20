2d77e96d33fd4f4dca8b3700b157958d55d5e6bc
vs_ <vs@athento.com>
2018-03-21 00:23:23 +0100

    Permanent delete operation

--- athento-nx-automation-extended/src/main/java/org/athento/nuxeo/operations/AthentoDocumentPermanentDeleteOperation.java
+++ athento-nx-automation-extended/src/main/java/org/athento/nuxeo/operations/AthentoDocumentPermanentDeleteOperation.java
@@ -20,18 +20,19 @@ import org.nuxeo.ecm.automation.core.collectors.DocumentModelCollector;
 import org.nuxeo.ecm.automation.core.util.DocumentHelper;
 import org.nuxeo.ecm.automation.core.util.Properties;
 import org.nuxeo.ecm.automation.core.util.StringList;
-import org.nuxeo.ecm.core.api.CoreSession;
-import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.api.*;
 import org.nuxeo.ecm.platform.tag.TagService;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.nuxeo.runtime.transaction.TransactionHelper.commitOrRollbackTransaction;
+import static org.nuxeo.runtime.transaction.TransactionHelper.startTransaction;
+
 /**
  * @author athento
  */
-@Operation(id = AthentoDocumentPermanentDeleteOperation.ID, category = "Athento", label = "Athento Document Update", description = "Updates a document in Athento's way")
+@Operation(id = AthentoDocumentPermanentDeleteOperation.ID, category = "Athento", label = "Athento Document Permanent Delete", description = "Delete a document in Athento's way")
 public class AthentoDocumentPermanentDeleteOperation extends AbstractAthentoOperation {
 
     /**
@@ -48,21 +49,59 @@ public class AthentoDocumentPermanentDeleteOperation extends AbstractAthentoOper
     @Context
     protected CoreSession session;
 
-    @Context
+    @Param(name = "docId", required = false)
     protected String docId;
+
+    /** Total deleted. */
+    private long total = 0;
 
     /**
      * Run.
      *
      * @throws Exception
      */
+    @OperationMethod()
     public void run() throws Exception {
+        if (docId == null){
+            throw new Exception("docId parameter is mandatory to remove the document");
+        }
         IdRef docRef = new IdRef(docId);
+        run(docRef);
+    }
+
+    /**
+     * Run.
+     *
+     * @throws Exception
+     */
+    @OperationMethod()
+    public void run(DocumentRef docRef) throws Exception {
         if (!session.canRemoveDocument(docRef)) {
             LOG.warn("Document " + docId + " cannot be removed.");
         } else {
-            LOG.info("Removing permanent for " + docId);
+            DocumentModel doc = session.getDocument(docRef);
+            if (doc.isFolder()) {
+                // Recursively
+                deleteFolder(doc.getRef());
+            }
             session.removeDocument(docRef);
+            total++;
+            LOG.info("Total removed = " + total);
+        }
+    }
+
+    private void deleteFolder(DocumentRef docRef) {
+        DocumentModelIterator it = session.getChildrenIterator(docRef);
+        while (it.hasNext()) {
+            DocumentModel document = it.next();
+            if (document.isFolder()) {
+                deleteFolder(document.getRef());
+            }
+            LOG.info("Removing permanent for " + document.getRef() + " (" + total + ")");
+            session.removeDocument(document.getRef());
+            commitOrRollbackTransaction();
+            startTransaction();
+            total++;
         }
     }
 
